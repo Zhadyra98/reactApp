18ba16ebf36d1f2a01219f283abd2e9d981d498e
Zhadyra Oralkhanova <oralkhanova.zhadyra@gmail.com>
2022-06-01 19:55:47 +0300

    feat: practice mini tasks for understanding components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+
+const Header = () => {
+  return <h2>First react app</h2>
+}
+
+// const Field1 = () => {
+
+  // const holder = 'Enter here';
+  // const styledField = {
+  //   width: '300px'
+  // };
+  // return <input 
+  //   placeholder={holder} 
+  //   type="text" 
+  //   style={styledField}>
+  //   </input>
+// }
+
+class Field extends Component{
+  render(){
+    const holder = 'Enter here';
+    const styledField = {
+      width: '300px'
+    };
+    return <input 
+        placeholder={holder} 
+        type="text" 
+        style={styledField}>
+        </input>
+  }
+}
+
+
+function Btn () {
+  const text ='Log in';
+  const isLogged = false;
+  // const res = () =>{
+  //   return 'Log in';
+  // }
+  return <button>{}</button>  
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-      </header>
+        <Header/>
+        <Field/>
+        <Btn/>
     </div>
   );
 }
 
+export {Header};
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Header } from './App';
+
+
+// const text ='hello';
+// const elem = (
+//   <div>
+//     <h2>ТексТ: {text}</h2>
+//     <input type="password" />
+//     <button tabIndex="0">Click</button>
+//   </div>
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App/>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
